c6bc82936e89113e6537c83f6dbb4196985eb472
skyefoxie <actuallyori@gmail.com>
2020-04-01 18:18:13 +0100

    attempt to fix injecction issue

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,114 +1,96 @@
-const { resolve } = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const { CheckerPlugin } = require("awesome-typescript-loader");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { resolve } = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CheckerPlugin } = require('awesome-typescript-loader');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 if (process.env.NODE_ENV === undefined) {
-	console.log("\n'NODE_ENV' is undefined - falling back to 'development'.\n");
-	process.env.NODE_ENV = "development";
+  console.log("\n'NODE_ENV' is undefined - falling back to 'development'.\n");
+  process.env.NODE_ENV = 'development';
 }
 
 module.exports = {
-	context: resolve(__dirname, "../src"),
-	entry: {
-		main: ["./index.tsx"],
-	},
-	output: {
-		filename: "[hash].js",
-		path: resolve(__dirname, "../dist"),
-		chunkFilename: "[contenthash].js",
-		publicPath: "/",
-	},
-	resolve: {
-		alias: {
-			assets: resolve(__dirname, "../src/assets/"),
-		},
-		extensions: [".ts", ".tsx", ".js", ".json"],
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: [
-					{
-						loader: "awesome-typescript-loader",
-						options: {
-							useBabel: true,
-							babelOptions: {
-								presets: ["@babel/preset-react"],
-								plugins: [
-									"@babel/plugin-syntax-dynamic-import",
-									"@babel/plugin-transform-modules-commonjs",
-									"react-hot-loader/babel",
-								],
-							},
-							babelCore: "@babel/core",
-						},
-					},
-				],
-				exclude: [resolve(__dirname, "node_modules")],
-			},
-			{
-				test: /\.less$/,
-				use: [
-					"style-loader",
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: process.env.NODE_ENV === "development",
-							reloadAll: true,
-						},
-					},
-					"css-loader",
-					"less-loader",
-				],
-			},
-			{
-				test: /\.css$/,
-				use: [
-					"style-loader",
-					{
-						loader: MiniCssExtractPlugin.loader,
-						options: {
-							hmr: process.env.NODE_ENV === "development",
-							reloadAll: true,
-						},
-					},
-					"css-loader",
-				],
-			},
-			{
-				test: /\.(png|webp|jpg)$/,
-				loader: "url-loader",
-				options: {
-					limit: 8192,
-				},
-			},
-			{
-				test: /\.(png|webp|jpg|svg)$/,
-				loader: "file-loader",
-			},
-		],
-	},
+  context: resolve(__dirname, '../src'),
+  entry: {
+    main: ['./index.tsx'],
+  },
+  output: {
+    filename: '[hash].js',
+    path: resolve(__dirname, '../dist'),
+    chunkFilename: '[contenthash].js',
+    publicPath: '/',
+  },
+  resolve: {
+    alias: {
+      assets: resolve(__dirname, '../src/assets/'),
+    },
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'awesome-typescript-loader',
+            options: {
+              useBabel: true,
+              useCache: true,
+              babelOptions: {
+                presets: ['@babel/preset-react'],
+                plugins: [
+                  '@babel/plugin-syntax-dynamic-import',
+                  '@babel/plugin-transform-modules-commonjs',
+                  'react-hot-loader/babel',
+                ],
+              },
+              babelCore: '@babel/core',
+            },
+          },
+        ],
+        exclude: [resolve(__dirname, 'node_modules')],
+      },
+      {
+        test: /\.(le|c)ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              javascriptEnabled: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|webp|jpg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
+      {
+        test: /\.(png|webp|jpe?g|svg)$/,
+        loader: 'file-loader',
+      },
+    ],
+  },
+
+  devtool: 'eval-source-map',
 };
 
 module.exports.plugins = [
-	new HtmlWebpackPlugin({
-		template: resolve(__dirname, "../src/public/index.html"),
-		title: "webpack-eslint-config",
-		filename: resolve(__dirname, "../dist/index.html"),
-	}),
+  new HtmlWebpackPlugin({
+    template: resolve(__dirname, '../src/public/index.html'),
+    title: 'webpack-eslint-config',
+    filename: resolve(__dirname, '../dist/index.html'),
+  }),
 
-	new webpack.DefinePlugin(require("./env")),
+  new webpack.DefinePlugin(require('./env')),
 
-	new MiniCssExtractPlugin({
-		filename: "[hash].css",
-		chunkFilename: "[contenthash].css",
-	}),
-	new CheckerPlugin(),
+  new CheckerPlugin(),
 
-	new webpack.HashedModuleIdsPlugin(),
-	new CleanWebpackPlugin(),
+  new webpack.NamedModulesPlugin(),
 ];
--- webpack/webpack.config.prod.js
+++ webpack/webpack.config.prod.js
@@ -1,33 +1,38 @@
-const webpack = require("webpack"),
-	TerserPlugin = require("terser-webpack-plugin"),
-	OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require('webpack'),
+  TerserPlugin = require('terser-webpack-plugin'),
+  OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin'),
+  MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const sharedWebpackConfig = require("./webpack.config");
+const sharedWebpackConfig = require('./webpack.config');
 
 const productionPlugins = [
-	new webpack.optimize.AggressiveMergingPlugin(),
-	new OptimizeCssAssetsPlugin(),
+  new webpack.optimize.AggressiveMergingPlugin(),
+  new OptimizeCssAssetsPlugin(),
+  new MiniCssExtractPlugin({
+    filename: '[hash].css',
+    chunkFilename: '[contenthash].css',
+  }),
 ];
 
 const productionConfig = {
-	mode: "production",
-	optimization: {
-		splitChunks: {
-			chunks: "all",
-			maxInitialRequests: Infinity,
-			maxSize: 512e3,
-			minSize: 0,
-			cacheGroups: {
-				vendor: {
-					test: /[\\/]node_modules[\\/]/,
-				},
-			},
-		},
-		minimize: true,
-		minimizer: [new TerserPlugin({ extractComments: true })],
-	},
+  mode: 'production',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      maxInitialRequests: Infinity,
+      maxSize: 512e3,
+      minSize: 0,
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+        },
+      },
+    },
+    minimize: true,
+    minimizer: [new TerserPlugin({ extractComments: true })],
+  },
 
-	plugins: sharedWebpackConfig.plugins.concat(productionPlugins),
+  plugins: sharedWebpackConfig.plugins.concat(productionPlugins),
 };
 
 module.exports = Object.assign({}, sharedWebpackConfig, productionConfig);
